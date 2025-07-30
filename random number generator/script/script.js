91def95e73a7e0f17380a1ca07620f1b9c318fff
Mauricio Farias da Silva <158978545+Mauricioibzde@users.noreply.github.com>
2025-07-30 14:34:59 +0200

    Update1.0

---
+++ random number generator/script/script.js
@@ -0,0 +1,30 @@
+const amount_01 = document.querySelector("input#amount-01")
+const amount_02 = document.querySelector("input#amount-02");
+const amount_03 = document.querySelector("input#amount-03")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
